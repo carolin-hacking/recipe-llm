2c133c508fc311f12ece2f35b0cff3221944942b
carolin-hacking <carolin.hecking@gmail.com>
2024-11-23 15:58:48 +0100

    Put recipe images in folders

--- src/transcription.py
+++ src/transcription.py
@@ -1,7 +1,7 @@
 import base64
 import json
 from pathlib import Path
-from typing import Union
+from typing import List, Union
 from openai import OpenAI
 from datetime import datetime
 
@@ -9,12 +9,10 @@ from tqdm import tqdm
 
 AVOID_API_CALLS = False
 
-def ask_question_about_image(image_path: Union[str, Path], question: str) -> str:
+def get_image_content_item(image_path: Union[str, Path]) -> str:
     if not isinstance(image_path, Path):
         image_path = Path(image_path)
     
-    if AVOID_API_CALLS:
-        return open("data/example_response.txt", "r").read()
     
     def encode_image(image_path) -> str:
         with open(image_path, "rb") as image_file:
@@ -22,6 +20,15 @@ def ask_question_about_image(image_path: Union[str, Path], question: str) -> str
 
     base64_image = encode_image(image_path)
     assert isinstance(base64_image, str)
+    return {"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{base64_image}"}}
+
+
+def ask_question_about_images(image_paths: Union[List[Union[str, Path]], Union[str, Path]], question: str) -> str:
+    """
+    Ask a question about multiple images.
+    """
+    if not isinstance(image_paths, list):
+        image_paths = [image_paths]
 
     client = OpenAI()
     response = client.chat.completions.create(
@@ -31,10 +38,7 @@ def ask_question_about_image(image_path: Union[str, Path], question: str) -> str
                 "role": "user",
                 "content": [
                     {"type": "text", "text": question},
-                    {
-                        "type": "image_url",
-                        "image_url": {"url": f"data:image/jpeg;base64,{base64_image}"}
-                    }
+                    *[get_image_content_item(image_path) for image_path in image_paths]
                 ]
             }
         ]
@@ -42,7 +46,7 @@ def ask_question_about_image(image_path: Union[str, Path], question: str) -> str
 
     response_info = {
         "question": question,
-        "image_name": image_path.name,
+        "image_names": [image_path.name for image_path in image_paths],
         "response": response.to_dict(),
         "answer": response.choices[0].message.content
     }
@@ -53,7 +57,7 @@ def ask_question_about_image(image_path: Union[str, Path], question: str) -> str
     return response.choices[0].message.content
 
 
-def transcribe_image(image_path: Union[str, Path]) -> str:
+def transcribe_image_set(image_paths: Union[List[Union[str, Path]], Union[str, Path]], output_file_name: str) -> str:
     """
     Transcribe the text in the image.
     
@@ -64,16 +68,16 @@ def transcribe_image(image_path: Union[str, Path]) -> str:
         The transcribed text.
     """
     Path("data/responses/transcriptions/").mkdir(parents=True, exist_ok=True)
-    question = "Please transcribe the text in the image."
-    answer = ask_question_about_image(image_path, question)
+    question = "Please transcribe the text in the images."
+    answer = ask_question_about_images(image_paths, question)
 
-    with open(f"data/responses/transcriptions/{image_path.name}.txt", "w") as file:
+    with open(f"data/responses/transcriptions/{output_file_name}.txt", "w") as file:
         file.write(answer)
     return answer
 
 
 if __name__ == "__main__":
-    for image_path in tqdm(Path("data/recipe_images").glob("*.jpg"), desc="Transcribing images in data/recipe_images"):
-        response_message = transcribe_image(image_path)
-
-        print(response_message)
+    for recipe_directory in Path("data/recipe_images").glob("*"):
+        if recipe_directory.is_dir():
+            images_of_recipe = list(Path(recipe_directory).glob("*.jpg"))
+            transcribe_image_set(images_of_recipe, f"recipe_{recipe_directory.name}")
