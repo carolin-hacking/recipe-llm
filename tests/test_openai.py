09005a9cf738c9541e5475fac0399b1796880e5f
Tomas Ruiz <tomas.ruiz.te@gmail.com>
2024-11-23 14:45:21 +0100

    Add a test to check that the OpenAI API works

--- tests/test_openai.py
+++ tests/test_openai.py
@@ -1,2 +1,23 @@
+from openai import OpenAI
+
+
 def test_openai_key_works():
-    assert True
+    """
+    From https://platform.openai.com/docs/quickstart?language-preference=python
+    Please set the env var OPENAI_API_KEY to your API key.
+    """
+    client = OpenAI()
+
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {
+                "role": "user",
+                "content": "Write a haiku about recursion in programming.",
+            },
+        ],
+    )
+
+    message: str = completion.choices[0].message.content
+    assert isinstance(message, str)
